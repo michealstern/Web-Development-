4f1778e7c5bdd641fede64451185bee44c978cab
Michial Stern <armankhan271609@gmail.com>
2024-08-29 11:56:02 +0530

    Add files via upload

---
+++ Rock-Paper-Scissors-Game/app.js
@@ -0,0 +1,70 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);  // ( math.random() *3 To Generate number between 0 to 2.) (math.floor to remove decimal value) 
+  return options[randIdx];
+};
+
+const drawGame = () => {
+  msg.innerText = "Game was Draw. Play again.";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userChoice) => {
+  //Generate computer choice
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    //Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      //scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      //rock, scissors
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      //rock, paper
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+choices.forEach((choice) => {   // To access all choices like: rock,paper & scissors.
+  choice.addEventListener("click", () => {   // To click choice.
+    const userChoice = choice.getAttribute("id"); // To access attributes like: rock,paper & scissors.
+    playGame(userChoice);  // For user choice.
+  });
+});
+
+
+// 1. Creat User choice function.
+// 2. Play game function.
+// 3. Generate Computer choice function.
+// 4. Draw Game.
+// 5. Show winner.
